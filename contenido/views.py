3ed47f4eb1021094f278943ec6cf493a91710ada
Sadaka1234 <gonzalo.duran.14@sansano.usm.cl>
2019-10-02 01:42:30 -0300

    Clase 5: Creada vista de 'Material'. Listo para enviar

--- contenido/views.py
+++ contenido/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from contenido.models import materia
+from preguntas.models import ejercicio, ejercicio_usuario
 from .forms import TopicoForm
 
 context = dict()
@@ -14,11 +15,18 @@ def material(request):
     if "cerrar" in request.POST.keys():
       auth.logout(request)
       return redirect("login")
+
     elif "materia" in request.POST.keys():
       context["submaterias"]=materia.objects.filter(topico=request.POST["materia"]).values("subtopico").order_by().distinct()
+      context["materia"]=request.POST["materia"]
+      return render(request, 'eleccion-submaterial.html', context)
+
     elif "submateria" in request.POST.keys():
-      context["mensaje"]="Buen trabajo, hiciste dos queries"
-  else:
-    form = TopicoForm()
+      actual = materia.objects.get(subtopico=request.POST["submateria"])
+      context["preguntas"]= ejercicio.objects.filter(materia=actual.id)
+      # Hay que crear el modelo de estudio context["preguntas"]=#
+      context["submateria"] = request.POST["submateria"]
+      return render(request, 'mostrar-material.html', context)
+
   context["materias"] = materia.objects.values("topico").order_by().distinct()
-  return render(request, 'material.html', context)
+  return render(request, 'eleccion-material.html', context)
